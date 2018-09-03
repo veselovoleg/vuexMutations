893d6bb0261c338a258385bd52d0e6d4290b8d0b
Веселов Олег Александрович <veselov.oleg@wildberries.ru>
2018-09-03 16:48:08 +0300

    modularizing the State managment

--- src/store/store.js
+++ src/store/store.js
@@ -2,54 +2,28 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+import counter from './modules/counter';
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0,
         value: 0
     },
     getters: {
-        doubleCounter: state => {
-            return state.counter * 2;
-        },
-        stringCounter: state => {
-            return state.counter + ' Clicks';
-        },
         value: state => {
             return state.value;
         }
     },
     mutations: {
-        increment: (state, payload) => {
-            state.counter += payload;
-        },
-        decrement: (state, payload) => {
-            state.counter -= payload;
-        },
         updateValue: (state, payload) => {
             state.value = payload;
         }
     },
     actions: {
-        increment: ({ commit }, payload) => {
-            commit('increment', payload);
-        },
-        decrement: ({ commit }, payload) => {
-            commit('decrement', payload);
-        },
-        asyncIncrement: ({ commit }, payload) => {
-            setTimeout(() => {
-                commit('increment', payload.by); 
-            }, payload.duration);
-        },
-        asyncDecrement: ({ commit }, payload) => {
-            setTimeout(() => {
-                commit('decrement', payload.by); 
-            }, payload.duration);
-        },
         updateValue({ commit }, payload) {
             commit('updateValue', payload);
-        }
-        
+        }    
+    },
+    modules: {
+        counter
     }
 });
